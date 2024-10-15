7a3de85a3c0bceaac0eff17e7df1c014a2a672c3
arturfrimu <artur@tecorien.com>
2024-10-15 19:59:02 +0300

    First commit with product microservice

---
+++ product-microservice/src/test/java/com/arturfrimu/cloud/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.arturfrimu.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
